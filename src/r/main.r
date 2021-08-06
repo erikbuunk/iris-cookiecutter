cbda7317170f15a73099b3f41069b32b3a56b3bb
Erik Buunk <erik.buunk@gmail.com>
2021-08-06 13:49:02 +0200

    small change

--- src/r/main.r
+++ src/r/main.r
@@ -1,3 +1,6 @@
+# Example Script for R
+
+# retrievee commandline argument for setting the working directory
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)>0) {
     setwd(args[1])
@@ -7,6 +10,6 @@ if (length(args)>0) {
 iris <- read.csv("data/orig/IRIS.csv")
 
 # create a simple plot
-pdf(file = "./results/figures/r_output.pdf")
+pdf(file = "results/figures/r_output.pdf")
 plot(iris$sepal_length, iris$sepal_width)
 dev.off()
